e0516098206805ba16d67e77b9649ea634dca78f
Jørn Larsen <jorn.larsen@jotun-utvikling.no>
2022-08-14 23:44:29 +0200

    :art: Update CSS

---
+++ js/conversations.js
@@ -0,0 +1,22 @@
+const conversation1 = {
+    user: "_Quoacky",
+    avatarUrl: "https://media-exp1.licdn.com/dms/image/D4D35AQFevl7ubMVLLQ/profile-framedphoto-shrink_200_200/0/1637567204999?e=1661119200&v=beta&t=dTgH8U1lBk7ADSl-Ni2ZVcH7-cOwtUlgywu0HxUAEso",
+    messages: [ 
+        {
+            "text": "Halla balla hva skjera?",
+        "user": {
+            name: "_Quoacky"
+        }},
+        {"text": "Lite, hva skjer der? SKal vi kode?",
+        "user": {
+            name: "Odi"
+        }
+
+},
+{
+            "text": "Greit la oss gjøre det",
+        "user": {
+            name: "_Quoacky"
+        }}
+    ]
+}
